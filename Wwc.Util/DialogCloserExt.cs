0d682db678d5ed00dba8611a1bb1110d313de73d
jjesus <john.j.jesus@gmail.com>
2014-09-05 16:48:18 -0500

    Initial.

---
+++ Wwc.Util/DialogCloserExt.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace Wwc.Util
+{
+    /// <summary>
+    /// A UI extension to close a dialog.
+    /// It works because if a dialog is opened using the showDialog() method,
+    /// the dialog is closed when its DialogResult property is set.
+    /// This extension helps to set that property from the ViewModel.
+    /// 
+    /// USAGE:
+    /// View
+    /// -----
+    ///     &lt;Window ...
+    ///         xmlns:ui="clr-namespace:Wwc.Util"
+    ///         ui:DialogCloserExt.DialogResult="{Binding DialogResult}"
+    ///         
+    /// ViewModel
+    /// -----
+    ///     Add a property for DialogResult, and, invoke OnPropertyChanged() when set
+    ///     
+    /// </summary>
+    public static class DialogCloserExt
+    {
+        public static readonly DependencyProperty DialogResultProperty =
+            DependencyProperty.RegisterAttached(
+                "DialogResult",
+                typeof(bool?),
+                typeof(DialogCloserExt),
+                new PropertyMetadata(DialogResultChanged));
+
+        private static void DialogResultChanged(
+            DependencyObject d, 
+            DependencyPropertyChangedEventArgs e)
+        {
+            var window = d as Window;
+            if (window != null) window.DialogResult = e.NewValue as bool?;
+        }
+
+        public static void SetDialogResult(Window target, bool? value)
+        {
+            target.SetValue(DialogResultProperty, value);
+        }
+    }
+}
---
+++ Wwc.Util/XmlEx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Wwc.Util
+{
+    public static class XmlEx
+    {
+        /// <summary>
+        /// If an XML element ("Name") is missing, then, doing something 
+        /// like this will throw an exception:
+        /// 
+        ///     xmlElement.Element("Name").Value
+        /// 
+        /// Instead of throwing an exception, this method returns a null string:
+        /// 
+        ///     xmlElement.SafeGetElementValue("Name")
+        /// 
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public static string SafeGetElementValue(this XElement parent, string element)
+        {
+            return
+                (parent != null) ?
+                    (parent.Element(element) != null) ?
+                            parent.Element(element).Value
+                            : null
+                    : null;
+        }
+    }
+}
